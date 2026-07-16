d4aa531368ce83b906ff7e518c4628be86912d6d
-JustACoder <josiah.seyller@gmail.com>
2026-07-16 16:22:49 -0400

    Added Geo-Coding

--- map.js
+++ map.js
@@ -1,1 +1,10 @@
-var map = L.map('map').setView([44.58207622, 103.461760283], 5);
+var map = L.map("map").setView([44.58207622, -103.461760283], 5);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {}).addTo(map);
+
+const provider = new GeoSearch.OpenStreetMapProvider();
+const searchControl = new GeoSearch.SearchControl({
+    provider,
+    style: "button",
+});
+
+map.addControl(searchControl)
